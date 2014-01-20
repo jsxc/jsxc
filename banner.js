3b19d587a3fa8cb45ef798aadbb19caa0fef2ab6
sualko <github@spam.herberth.eu>
2014-01-20 13:41:43 +0100

    add banner and add ignore

---
+++ banner.js
@@ -0,0 +1,11 @@
+/**
+ * <%= app.name %> v<%= app.version %> - <%= grunt.template.today("yyyy-mm-dd") %>
+ * 
+ * Copyright (c) <%= grunt.template.today("yyyy") %> <%= app.author %> <br>
+ * Released under the <%= app.license %> license
+ * 
+ * Please see <%= app.homepage %>
+ * 
+ * @author <%= app.author %>
+ * @version <%= app.version %>
+ */
