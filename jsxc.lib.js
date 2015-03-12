9651e2c7fe9af97e6ba60530ee18a31a73efbae4
Augier <contact@c-henry.fr>
2015-03-12 22:48:54 +0100

    Revert changes introduced in 0.1.0'

--- jsxc.lib.js
+++ jsxc.lib.js
@@ -1032,15 +1032,21 @@ var jsxc;
          var we = jsxc.gui.window.get(bid); // window element from user
          var ue = ri.add(we); // both
          var spot = $('.jsxc_spot[data-bid="' + bid + '"]');
+         var data_name = $('<a href="/u/' + data.jid + '" target="_blank"></a>').text(data.name);
 
          // Attach data to corresponding roster item
          ri.data(data);
 
          // Add online status
-         ue.add(spot).removeClass('jsxc_' + jsxc.CONST.STATUS.join(' jsxc_')).addClass('jsxc_' + jsxc.CONST.STATUS[data.status]);
+         ri.add(spot).removeClass('jsxc_' + jsxc.CONST.STATUS.join(' jsxc_')).addClass('jsxc_' + jsxc.CONST.STATUS[data.status]);
+         we.find('.jsxc_presence_badge')
+            .removeClass().addClass('jsxc_presence_badge jsxc_badge_' + jsxc.CONST.STATUS[data.status])
+            .attr('title', $.t("is") + ' ' + jsxc.CONST.STATUS[data.status]);
 
          // Change name and add title
-         ue.find('.jsxc_name').add(spot).text(data.name).attr('title', $.t("is") + ' ' + jsxc.CONST.STATUS[data.status]);
+         ue.find('.jsxc_name').add(spot).text(data.name);
+         ue.find('#jsxc_window_name').add(spot).attr('title', $.t("is") + ' ' + jsxc.CONST.STATUS[data.status]);
+         ue.find('#jsxc_window_name').empty().append(data_name);
 
          // Update gui according to encryption state
          switch (data.msgstate) {
@@ -2541,8 +2547,14 @@ var jsxc;
             jsxc.otr.toggleTransfer(bid);
          });
 
-         win.find('.jsxc_bar').click(function() {
+         win.find('.jsxc_reduce').click(function() {
             jsxc.gui.window.toggle(bid);
+            win.find('.jsxc_cycle').css('display', 'none');
+         });
+
+         win.find('.jsxc_restore').click(function() {
+            jsxc.gui.window.toggle(bid);
+            win.find('.jsxc_cycle').css('display', '');
          });
 
          win.find('.jsxc_close').click(function() {
@@ -3142,6 +3154,7 @@ var jsxc;
                 <div class="jsxc_bar">\
                      <div class="jsxc_avatar">☺</div>\
                      <div class="jsxc_tools">\
+                           <div class="jsxc_presence_badge"></div>\
                            <div class="jsxc_settings">\
                                <ul>\
                                    <li class="jsxc_fingerprints jsxc_otr jsxc_disabled">%%Fingerprints%%</li>\
@@ -3151,6 +3164,7 @@ var jsxc;
                                </ul>\
                            </div>\
                            <div class="jsxc_transfer jsxc_otr jsxc_disabled"/>\
+                           <div class="jsxc_reduce">–</div>\
                            <div class="jsxc_close">×</div>\
                      </div>\
                      <div class="jsxc_name"/>\
@@ -3211,7 +3225,8 @@ var jsxc;
       rosterBuddy: '<li>\
             <div class="jsxc_avatar">☺</div>\
             <div class="jsxc_control"></div>\
-            <div class="jsxc_name"/>\
+            <div class="jsxc_restore">■</div>\
+            <div id="jsxc_window_name" nameclass="jsxc_name"/>\
             <div class="jsxc_options jsxc_right">\
                 <div class="jsxc_rename" title="%%rename_buddy%%">✎</div>\
                 <div class="jsxc_delete" title="%%delete_buddy%%">✘</div>\
