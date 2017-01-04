a39f7d504cf0c3204bfb5a932fb485eec3b77c37
Klaus <klaus@jsxc.org>
2017-01-04 16:21:21 +0100

    Revert "New feature: notification of a message composing in a chat/groupchat window"

--- src/jsxc.lib.js
+++ src/jsxc.lib.js
@@ -25,9 +25,6 @@ jsxc = {
    /** Timeout delay for notification */
    toNotificationDelay: 500,
 
-   /** Delay between two notification on the message composing */
-   toComposingNotificationDelay: 900,
-
    /** Interval for keep-alive */
    keepaliveInterval: null,
 
