b5d5e6c6475a806122188e52ccaae3093a96cefb
singh1114 <ranvir.singh1114@gmail.com>
2017-01-11 17:09:30 +0530

    adds the file src/jsxc.lib.xmpp.mam.js

---
+++ src/jsxc.lib.xmpp.mam.js
@@ -0,0 +1,17 @@
+/**
+ * Implements XEP-0085: Chat State Notifications.
+ *
+ * @namespace jsxc.xmpp.mam
+ * @see {@link http://xmpp.org/extensions/xep-0313.html}
+*/
+jsxc.xmpp.mam = {
+  conn: null,
+
+  /** Total number of messages to be retrived in one time */
+  messageRetreived: 100,
+};
+
+jsxc.xmpp.mam.init = function(){
+  var self = jsxc.xmpp.mam;
+
+}
