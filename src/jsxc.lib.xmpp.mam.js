1f9701c8c36142da2ea6cf22b48f2dd9388e00cd
singh1114 <ranvir.singh1114@gmail.com>
2017-01-12 15:49:55 +0530

    changes file name

--- src/jsxc.lib.xmpp.mam.js
+++ src/jsxc.lib.xmpp.mam.js
@@ -8,10 +8,20 @@ jsxc.xmpp.mam = {
   conn: null,
 
   /** Total number of messages to be retrived in one time */
-  messageRetreived: 100,
+  messageRetreivedInOneGO: 100,
 };
 
 jsxc.xmpp.mam.init = function(){
   var self = jsxc.xmpp.mam;
+
+}
+
+jsxc.xmpp.mam.getHistory = function(jid, count, before){
+  var self = jsxc.xmpp.mam;
+  var bid = jsxc.jidToBid(jid);
+
+  jsxc.xmpp.mam.onMessage = function() {
+    
+  }
 
 }
