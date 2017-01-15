8fd3e888dd768a4bcfb8c534599498260260e19c
Abhay Yadav <abhayyadav000@gmail.com>
2017-01-15 17:40:16 +0530

    Adds general policy and code structure
    It adds some example code to fetch the message archive. It also discusses the general policy to be followed.

--- src/jsxc.lib.xmpp.mam.js
+++ src/jsxc.lib.xmpp.mam.js
@@ -1,27 +1,97 @@
 /**
- * Implements XEP-0085: Chat State Notifications.
+ * Implements XEP-0313: Messge Archive Management
  *
  * @namespace jsxc.xmpp.mam
  * @see {@link http://xmpp.org/extensions/xep-0313.html}
-*/
-jsxc.xmpp.mam = {
-  conn: null,
+ */
 
-  /** Total number of messages to be retrived in one time */
-  messageRetreivedInOneGO: 100,
+/**
+ *General policy is to load the history when a chat box or room opens up.
+ *The user will firstly request for n messages and when it scrolls
+ *up and reaches the end, then it again request for another n messages.
+ *It goes on further untill no message is left in the archive. The
+ *server replies with no more message in the history
+ *
+ *archivedMessagePageSize = '20' //No. of messages in one page
+ *
+ *rsmAttributes = ['max', 'first', 'last', 'after', 'before', 
+ *'index', 'count']
+ *
+ *mamAttributes = ['with', 'start', 'end']
+ *
+ *messageArchiving = 'never' // Supported values are 'always', 'never',
+ * 'roster' (See https://xmpp.org/extensions/xep-0313.html#prefs )
+ *
+ *mucHistoryMaxStanzas: undefined, // Takes an integer, limits the 
+ *amount of messages to fetch from chat room's history
+ *
+ */
+
+/*jsxc.xmpp.mam = {
+  conn: jsxc.xmpp.conn
 };
 
 jsxc.xmpp.mam.init = function(){
   var self = jsxc.xmpp.mam;
+  self.getHistory(userid, count, before);
+};*/
 
-}
+//example code to query an personal archive for conversations with 
+//juliet@capulet.com
 
-jsxc.xmpp.mam.getHistory = function(jid, count, before){
-  var self = jsxc.xmpp.mam;
-  var bid = jsxc.jidToBid(jid);
-
-  jsxc.xmpp.mam.onMessage = function() {
-    
+/*
+connection.mam.query("you@example.com", {
+  "with": "juliet@capulet.com",
+  onMessage: function(message) {
+            console.log("Message from ", $(message).find("forwarded message").attr("from"),
+                ": ", $(message).find("forwarded message body").text());
+            return true;
+  },
+  onComplete: function(response) {
+            console.log("Got all the messages");
   }
+    });
+*/
 
-}
+
+/*jsxc.xmpp.mam.getHistory = function (userid, count, before){
+    var q = {
+      onMessage: function(message) {
+        //attach a code in here to view the message in the chat window
+
+        var id = message.querySelector('result').getAttribute('id');
+        var fwd = message.querySelector('forwarded');
+        var d = fwd.querySelector('delay').getAttribute('stamp');
+        var msg = fwd.querySelector('message');
+        var msg_data = {
+            id:id,
+            timestamp: (new Date(d)),
+            timestamp_orig: d,
+            from: Strophe.getBareJidFromJid(msg.getAttribute('from')),
+            to: Strophe.getBareJidFromJid(msg.getAttribute('to')),
+            type: msg.getAttribute('type'),
+            body: msg.getAttribute('body'),
+            message: Strophe.getText(msg.getElementsByTagName('body')[0])
+        };
+
+        var sender = Strophe.getBareJidFromJid(msg_data.from);
+
+        console.log(d + ': ' + sender + ' said: ' + msg_data.message + '<br>');
+        return true;
+      },
+
+      onComplete: function(response) {
+        //attach a code in here to notify when all the message has reached
+        
+    	console.log(response);
+        return true;
+      }
+    };
+
+    $.extend(q, {'with': userid, 'before': before, 'max': count});
+
+    jsxc.xmpp.conn.mam.query(jsxc.bid, q);
+  }
+};
+*/
+  
