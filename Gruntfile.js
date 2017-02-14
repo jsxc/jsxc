daa5d35d7a871d960a6e0f8eb5571bec34efd9d1
sualko <klaus@jsxc.org>
2017-02-14 16:19:38 +0100

    check for missing files in build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,7 +155,15 @@ module.exports = function(grunt) {
                }
             },
             src: dep_files,
-            dest: '<%= target %>/lib/jsxc.dep.js'
+            dest: '<%= target %>/lib/jsxc.dep.js',
+            filter: function (filepath) {
+                if (!grunt.file.exists(filepath)) {
+                    grunt.fail.warn('Could not find: ' + filepath);
+                } else {
+                    return true;
+                }
+            },
+            nonull: true,
          },
          jsxc: {
             options: {
