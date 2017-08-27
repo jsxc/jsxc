34b7888da69a7ef54b4a303bf2e13a388a1b46f6
sualko <klaus@jsxc.org>
2017-08-27 15:20:19 +0200

    add pipes

---
+++ src/Pipe.ts
@@ -0,0 +1,51 @@
+
+const MAX_PRIORITY = 100;
+const MIN_PRIORITY = 0;
+
+export default class Pipe {
+   private static instances = {};
+
+   public static get(name:string):Pipe {
+      if (typeof Pipe.instances[name] === 'undefined') {
+         Pipe.instances[name] = new Pipe();
+      }
+
+      return Pipe.instances[name];
+   }
+
+   private pipe = [];
+
+   constructor() {
+
+   }
+
+   public addProcessor(processor:(...args) => Promise<any>|Array<any>, priority:number = 50) {
+      if(isNaN(priority) || priority < MIN_PRIORITY || priority > MAX_PRIORITY) {
+         throw 'Priority has to be between 0 and 100';
+      }
+
+      if (typeof this.pipe[priority] === 'undefined') {
+         this.pipe[priority] = [];
+      }
+
+      this.pipe[priority].push(processor);
+   }
+
+   public run(...args) {
+      let chain = Promise.resolve(args);
+
+      this.pipe.forEach((processors) => {
+         if (typeof processors === 'undefined' || processors === null || typeof processors !== 'object' || !processors.length) {
+            return;
+         }
+
+         processors.forEach((processor) => {
+            chain = chain.then((args2:Array<any>) => {
+               return processor.apply(this, args2);
+            });
+         });
+      });
+
+      return chain;
+   }
+}
---
+++ test/Pipe.spec.ts
@@ -0,0 +1,91 @@
+import {expect} from 'chai';
+import * as sinon from 'sinon';
+import 'mocha';
+
+import Pipe from '../src/Pipe'
+
+describe('Pipe', () => {
+   it('should return the same pipe for the same name', () => {
+      let pipeA = Pipe.get('dummy');
+      let pipeB = Pipe.get('dummy');
+
+      expect(pipeA).equals(pipeB);
+   })
+
+   it('should return different pipes for different names', () => {
+      let pipeA = Pipe.get('dummyA');
+      let pipeB = Pipe.get('dummyB');
+
+      expect(pipeA).not.equals(pipeB);
+   })
+
+   it('should run an empty pipe', () => {
+      let pipeA = Pipe.get('dummy_empty');
+
+      return pipeA.run('A', 'B', 1234).then(([arg1, arg2, arg3]) => {
+         expect(arg1).equals('A');
+         expect(arg2).equals('B');
+         expect(arg3).equals(1234);
+      });
+   })
+
+   it('should progress in order with distinct priority', () => {
+      let pipeA = Pipe.get('dummy_distinct');
+
+      pipeA.addProcessor(function(a, b, c) {
+         return Promise.resolve([a + 'a', b + 'b', c * 2]);
+      }, 20);
+
+      pipeA.addProcessor(function(a, b, c) {
+         return Promise.resolve([a + 'c', b + 'd', c + 1]);
+      }, 30);
+
+      pipeA.addProcessor(function(a, b, c){
+         return Promise.resolve([a + 'e', b + 'f', c * 3]);
+      }, 80);
+
+      return pipeA.run('A', 'B', 1234).then(([arg1, arg2, arg3]) => {
+         expect(arg1).equals('Aace');
+         expect(arg2).equals('Bbdf');
+         expect(arg3).equals(7407);
+      });
+   })
+
+   it('should progress in order with multiple priority', () => {
+      let pipeA = Pipe.get('dummy_multiple');
+
+      pipeA.addProcessor(function(a, b, c){
+         return [a + 'a', b + 'b', c * 2];
+      }, 20);
+
+      pipeA.addProcessor(function(a, b, c){
+         return Promise.resolve([a + 'c', b + 'd', c + 1]);
+      }, 20);
+
+      pipeA.addProcessor(function(a, b, c){
+         return Promise.resolve([a + 'e', b + 'f', c * 3]);
+      }, 20);
+
+      return pipeA.run('A', 'B', 1234).then(([arg1, arg2, arg3]) => {
+         expect(arg1).equals('Aace');
+         expect(arg2).equals('Bbdf');
+         expect(arg3).equals(7407);
+      });
+   })
+
+   it('should reject processor with invalid priority', () => {
+      let pipeA = Pipe.get('dummy_invalid');
+
+      expect(() => {
+         pipeA.addProcessor(function(...args){return Promise.resolve();}, -1);
+      }).to.throw();
+
+      expect(() => {
+         pipeA.addProcessor(function(...args){return Promise.resolve();}, 101);
+      }).to.throw();
+
+      expect(() => {
+         pipeA.addProcessor(function(...args){return Promise.resolve();}, 'abc');
+      }).to.throw();
+   })
+});
