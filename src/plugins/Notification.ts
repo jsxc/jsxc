aa44186bc2255293c0113489a73f212c47b0de73
sualko <klaus@jsxc.org>
2017-09-01 18:06:30 +0200

    add notification plugin

---
+++ src/plugins/Notification.ts
@@ -0,0 +1,40 @@
+import Client from '../Client'
+import Options from '../Options'
+import * as CONST from '../CONST'
+import Message from '../Message'
+import {AbstractPlugin} from '../plugin/AbstractPlugin'
+import PluginAPI from '../plugin/PluginAPI'
+import Pipe from '../Pipe'
+import Contact from '../Contact'
+import Translation from '../util/Translation'
+import Notification from '../Notification'
+import {SOUNDS} from '../CONST'
+import * as Namespace from '../connection/xmpp/namespace'
+
+const MIN_VERSION = '4.0.0';
+const MAX_VERSION = '4.0.0';
+
+export default class NotificationPlugin extends AbstractPlugin {
+
+   public static getName():string {
+      return 'notification';
+   }
+
+   constructor(pluginAPI:PluginAPI) {
+      super(MIN_VERSION, MAX_VERSION, pluginAPI);
+
+      let pipe = Pipe.get('afterReceiveMessage');
+      pipe.addProcessor(this.afterReceiveMessageProcessor, 90);
+   }
+
+   private afterReceiveMessageProcessor = (contact:Contact, message:Message) => { console.log('armp', message)
+      Notification.notify({
+         title: Translation.t('New_message_from'),
+         message: message.getProcessedBody(),
+         soundFile: SOUNDS.MSG,
+         source: contact
+      });
+
+      return [contact, message];
+   }
+}
