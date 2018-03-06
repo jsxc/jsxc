c560b9110fd085bdd1dd3761f09d71c69a388e44
sualko <klaus@jsxc.org>
2018-03-06 11:10:58 +0100

    extend log functions

--- src/util/Log.ts
+++ src/util/Log.ts
@@ -8,31 +8,31 @@ enum LogLevel {
 };
 
 export default class Log {
-   public static info(message: string, data?: any): void {
-      Log.log(LogLevel.Info, message, data);
+   public static info(message: string, ...data): void {
+      Log.log(LogLevel.Info, message, ...data);
    }
 
-   public static debug(message: string, data?: any): void {
-      Log.log(LogLevel.Debug, message, data);
+   public static debug(message: string, ...data): void {
+      Log.log(LogLevel.Debug, message, ...data);
    }
 
-   public static warn(message: string, data?: any): void {
-      Log.log(LogLevel.Warn, message, data);
+   public static warn(message: string, ...data): void {
+      Log.log(LogLevel.Warn, message, ...data);
    }
 
-   public static error(message: string, data?: any): void {
-      Log.log(LogLevel.Error, message, data);
+   public static error(message: string, ...data): void {
+      Log.log(LogLevel.Error, message, ...data);
    }
 
    private static getPrefix(level: LogLevel): string {
       return '[' + LogLevel[level] + '] ';
    }
 
-   private static log(level: LogLevel, message: string, data?: any) {
+   private static log(level: LogLevel, message: string, ...data) {
       if (/*Client.isDebugMode() && */ typeof console !== 'undefined') {
          let logFunction = (level === LogLevel.Warn || level === LogLevel.Error) ? 'warn' : 'log';
 
-         console[logFunction](Log.getPrefix(level) + message, data);
+         console[logFunction].apply(this, [Log.getPrefix(level) + message, ...data]);
       }
    }
 }
