2b13c018b950a34308722b94890eeafae27a55ce
sualko <klaus@jsxc.org>
2019-05-24 14:48:14 +0200

    feat: adapt group chat message bg
    to sender

--- src/util/Color.ts
+++ src/util/Color.ts
@@ -1,13 +1,8 @@
 import * as getRGB from 'consistent-color-generation'
 
 export default class Color {
-   public static generate(text: string /*, saturation: number = 90, lightness: number = 65*/) {
-      // let hash = Hash.String(text);
-
-      // let hue = Math.abs(hash) % 360;
-      // let hsl = 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%)';
-
-      let color = getRGB(text);
+   public static generate(text: string, correction?: 'redgreen' | 'blue', saturation?: number, lightness?: number) {
+      let color = getRGB(text, correction, saturation, lightness);
       let r = Math.round(color.r * 255);
       let g = Math.round(color.g * 255);
       let b = Math.round(color.b * 255);
