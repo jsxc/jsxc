55d713a626a4ca1c0861376b1aa1b6462debc3f8
sualko <klaus@jsxc.org>
2020-03-23 14:07:31 +0100

    fix: handle empty server features
    fix #836

--- src/connection/xmpp/Connector.ts
+++ src/connection/xmpp/Connector.ts
@@ -171,6 +171,10 @@ export default class Connector {
       let from = new JID('', connection.domain, '');
       let stanza = connection.features;
 
+      if (!stanza) {
+         return;
+      }
+
       let capsElement = stanza.querySelector('c');
 
       if (!capsElement) {
