929683efd11b950d292cbc1f4f887ee056fac95d
sualko <klaus@jsxc.org>
2020-04-29 15:16:10 +0200

    chore: improve pre commit hook

--- scripts/print-commitlint-info.js
+++
@@ -1,8 +0,0 @@
-require('colors');
-const config = require('../.commitlintrc.json');
-
-console.log(`
-✖   See https://www.conventionalcommits.org/ for more details.
-    Allowed types: ${config.rules['type-enum'][2].join(', ')}`.red);
-
-process.exit(1);
