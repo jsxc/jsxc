3a3078d462a6e47a7ea738ffc68fcbf974e1fc65
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-07-24 17:07:51 +0200

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { ThemeProvider } from '@chakra-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Landing, Login } from './pages';
+import { Header } from './components';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider>
       <BrowserRouter>
+        <Header />
+
         <Switch>
           <Route path="/" exact={true} component={Landing} />
           <Route path="/login" component={Login} />
