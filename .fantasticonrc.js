32ad2eabc504be8a60d33077a1f4b78c4087d0c3
sualko <klaus@jsxc.org>
2021-04-15 17:12:19 +0200

    chore: add config for fantasticon

---
+++ .fantasticonrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    inputDir: './images/icons/',
+    outputDir: './fonts/',
+    assetTypes: ['scss'],
+    name: 'jsxc-icons',
+    normalize: true,
+    tag: '',
+    prefix: 'jsxc-icon',
+    fontsUrl: '../fonts',
+};
