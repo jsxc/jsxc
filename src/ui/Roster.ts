3ce45bc0ba06e07ef9a8af0f46d7e7193df766fe
mightymop <totzkotz@gmail.com>
2021-11-23 09:46:49 +0100

    style: rearange menu buttons

--- src/ui/Roster.ts
+++ src/ui/Roster.ts
@@ -367,17 +367,18 @@ export default class Roster {
       }
 
       this.addMenuEntry({
-         id: 'search-contact',
-         handler: ()=> showContactSearchDialog(),
-         label: Translation.t('contact_search'),
-         icon: 'search',
+         id: 'settings',
+         handler: showSettingsDialog,
+         label: Translation.t('Settings'),
+         offlineAvailable: true,
+         icon: 'gear',
       });
 
       this.addMenuEntry({
-         id: 'add-contact',
-         handler: () => showContactDialog(),
-         label: Translation.t('Add_buddy'),
-         icon: 'contact',
+         id: 'add-avatar',
+         handler: showAddAvatarDialog,
+         label: Translation.t('Edit_avatar'),
+         icon: 'smiley'
       });
 
       this.addMenuEntry({
@@ -394,26 +395,26 @@ export default class Roster {
       });
 
       this.addMenuEntry({
+         id: 'search-contact',
+         handler: ()=> showContactSearchDialog(),
+         label: Translation.t('contact_search'),
+         icon: 'search',
+      });
+
+      this.addMenuEntry({
+         id: 'add-contact',
+         handler: () => showContactDialog(),
+         label: Translation.t('Add_buddy'),
+         icon: 'contact',
+      });
+
+      this.addMenuEntry({
          id: 'join-muc',
          handler: () => showMultiUserJoinDialog(),
          label: Translation.t('Join_chat'),
          icon: 'groupcontact',
       });
-
-      this.addMenuEntry({
-         id: 'settings',
-         handler: showSettingsDialog,
-         label: Translation.t('Settings'),
-         offlineAvailable: true,
-         icon: 'gear',
-      });
-
-      this.addMenuEntry({
-         id: 'add-avatar',
-         handler: showAddAvatarDialog,
-         label: Translation.t('Edit_avatar'),
-         icon: 'smiley'
-      });
+     
    }
 
    private registerPresenceHandler() {
