7da71435b46971f150b111f626cc9ddaa7a948a6
mightymop <totzkotz@gmail.com>
2022-02-27 03:32:02 +0100

    fix: several fixes for muc invitations and hide unnecessary fields in invitation dialog

--- src/connection/xmpp/handlers/multiUser/XMessage.ts
+++ src/connection/xmpp/handlers/multiUser/XMessage.ts
@@ -1,6 +1,7 @@
 import JID from '../../../../JID';
 import AbstractHandler from '../../AbstractHandler';
-import { TYPE as NOTICETYPE, FUNCTION as NOTICEFUNCTION } from '../../../../Notice';
+import { TYPE as NOTICETYPE, FUNCTION as NOTICEFUNCTION, Notice } from '../../../../Notice';
+import Translation from '@util/Translation';
 
 export default class extends AbstractHandler {
    public processStanza(stanza: Element) {
@@ -13,14 +14,22 @@ export default class extends AbstractHandler {
          let host = new JID(inviteElement.attr('from'));
          let reason = inviteElement.find('reason').text();
          let password = inviteElement.find('password').text();
-
-         this.account.getNoticeManager().addNotice({
-            title: 'Invitation',
-            description: `for ${from.bare}`,
-            type: NOTICETYPE.invitation,
-            fnName: NOTICEFUNCTION.multiUserInvitation,
-            fnParams: ['direct', host.bare, from.bare, reason, password, this.account.getUid()],
-         });
+         let lastnoti: Notice = this.account.getNoticeManager().getNotices().getLastItem();
+         if (
+            lastnoti === null ||
+            lastnoti.getTitle() !== Translation.t('muc_invitation') ||
+            lastnoti.getDescription() !== `for ${from.bare}` ||
+            lastnoti.getType() !== NOTICETYPE.invitation ||
+            lastnoti.callFunction() !== NOTICEFUNCTION.multiUserInvitation
+         ) {
+            this.account.getNoticeManager().addNotice({
+               title: Translation.t('muc_invitation'),
+               description: `for ${from.bare}`,
+               type: NOTICETYPE.invitation,
+               fnName: NOTICEFUNCTION.multiUserInvitation,
+               fnParams: ['direct', host.bare, from.bare, reason, password, this.account.getUid()],
+            });
+         }
       }
 
       return this.PRESERVE_HANDLER;
